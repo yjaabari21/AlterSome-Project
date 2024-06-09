2f278e1c1c71e4feaf478f402b63d86dcb5cb7f1
Yousuf <74505346+yjaabari21@users.noreply.github.com>
2024-06-09 19:07:11 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using AlterSome.Models;
+
+namespace AlterSome.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+		
+		public ActionResult List()
+		{
+			using (var cxt = new AlterSomeEntities())
+			{
+				var data = cxt.Alters.AsQueryable().OrderByDescending(x => x.Id).ToList();
+				List<Alter> list = new List<Alter>();
+				foreach (var item in data)
+                {
+					list.Add(
+						new Alter
+						{
+							Id = item.Id,
+							ImageApp = item.ImageApp,
+							Name = item.Name,
+							LinkUrl = item.LinkUrl,
+							//LinkUrl2 = item.LinkUrl2,
+							//LinkUrl3 = item.LinkUrl3,
+							Description = item.Description,
+						}
+						);
+                };
+				return View(list);
+            }
+		}
+	}
+}
